533b260a8048d566f29a9f985511d5859d841380
Levente <Levente@DESKTOP-4G9FPUD>
2024-12-17 12:09:53 +0100

    Add project files.

---
+++ 12.17/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._17
+{
+    internal class Program
+    {
+
+        static void F01(int[] szamok)
+        {
+            Random random = new Random();
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                szamok[i] = random.Next(1, 51);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] szamok = new int[10];
+            F01(szamok);
+            F05(szamok);
+
+        }
+    }
+}
